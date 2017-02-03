5d70e6fcedf7db88e809d254d0f8558a86f0a81c
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-02-03 11:54:01 -0700

    added js linting

--- models/list.js
+++ models/list.js
@@ -1,11 +1,9 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var listSchema = mongoose.Schema({
-
-    'owner'       : String,
-    'name'        : String,
-    'description' : String
-
+const listSchema = mongoose.Schema({
+  owner: String,
+  name: String,
+  description: String,
 });
 
 module.exports = mongoose.model('List', listSchema);
