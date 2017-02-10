dd9db4c3815465acbd907f16340e94aa8b55bf01
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-02-09 23:27:31 -0700

    more tests for health check

--- test/healthCheck.js
+++ test/healthCheck.js
@@ -2,7 +2,13 @@ const request = require('supertest');
 const app = require('../index.js');
 
 describe('Health Check', () => {
-  it('Should be status code 200', (done) => {
-    request(app).get('/health').expect(200, done);
+  it('Status code 200', (done) => {
+    request(app).get('/health').expect(200, done)
+  });
+  it('Content-Type json', (done) => {
+    request(app).get('/health').expect('Content-Type', 'application/json; charset=utf-8', done);
+  });
+  it('Returns {STATUS: UP}', (done) => {
+    request(app).get('/health').expect({STATUS: 'UP'}, done);
   });
 });
