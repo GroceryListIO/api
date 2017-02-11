37ff4cbd5cf39a7a6f62523d7ecfd1f065787d3d
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-02-11 02:24:21 -0700

    send description

--- test/lists.js
+++ test/lists.js
@@ -7,14 +7,14 @@ describe('Lists', () => {
   it('GET /lists - Get All Lists', (done) => {
     request(app)
     .get('/lists')
-    .expect(200)
-    .expect('Content-Type', 'application/json; charset=utf-8', done);
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .expect(200, done);
   });
 
   it('POST /lists - Create A List', (done) => {
     request(app)
     .post('/lists')
-    .send({ name: 'Test POST' })
+    .send({ name: 'Test POST', description: 'Unit Test' })
     .expect('Content-Type', 'application/json; charset=utf-8')
     .expect(200, done);
   });
