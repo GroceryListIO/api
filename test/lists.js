783e208516740d69dbfa789ecf30516fa83da762
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-02-19 11:02:25 -0700

    working async tests

--- test/lists.js
+++ test/lists.js
@@ -1,22 +1,46 @@
 process.env.ENV = 'Test';
 const request = require('supertest');
 const app = require('../index.js');
+const async = require('async');
+
+let testList = {};
 
 describe('Lists', () => {
+  async.series([
 
-  it('GET /lists - Get All Lists', (done) => {
-    request(app)
-    .get('/lists')
-    .expect('Content-Type', 'application/json; charset=utf-8')
-    .expect(200, done);
-  });
+    function createAList(asyncDone) {
+      it('POST /lists - Create A List', (done) => {
+        request(app)
+        .post('/lists')
+        .send({ name: 'Test POST', description: 'Unit Test' })
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end( (err, res) => {
+          testList = res.body;
+          done();
+        });
+      })
+      asyncDone();
+    },
 
-  it('POST /lists - Create A List', (done) => {
-    request(app)
-    .post('/lists')
-    .send({ name: 'Test POST', description: 'Unit Test' })
-    .expect('Content-Type', 'application/json; charset=utf-8')
-    .expect(200, done);
-  });
+    function getAllLists(asyncDone) {
+      it('GET /lists - Get All Lists', (done) => {
+        request(app)
+        .get('/lists')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+      })
+      asyncDone();
+    },
 
+    function deleteList(asyncDone) {
+      it('DELETE /lists/:listID - Delete A List', (done) => {
+        request(app)
+        .del('/lists/' + testList._id)
+        .expect(200, done);
+      })
+      asyncDone();
+    },
+
+  ]);
 });
