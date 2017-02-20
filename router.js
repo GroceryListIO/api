fe82bb3e5f9d1d71330d481f01308f9985adc529
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-02-20 16:47:42 -0700

    auth routes on item endpoints

--- router.js
+++ router.js
@@ -1,21 +1,20 @@
 const passport = require('passport');
 require('./config/passport');
 
-// Controllers. TODO: Movee everything to controller style
+// Controllers.
 const authController = require('./controllers/authentication');
 const listsController = require('./controllers/lists');
+const itemController = require('./controllers/items');
 
-// Routes. Simpler route style endpoints.
+// Routers. Simpler route style endpoints.
 const healthCheckRoutes = require('./routes/healthCheck');
-const itemRoutes = require('./routes/items');
 
 // Middleware to require login/auth
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 
+// Attach Routes
 module.exports = (app) => {
-  // Routes //
-
   // Defaults
   app.use(healthCheckRoutes);
 
@@ -29,5 +28,9 @@ module.exports = (app) => {
   app.get('/lists/:id', requireAuth, listsController.getList);
   app.delete('/lists/:id', requireAuth, listsController.deleteList);
 
-  app.use(itemRoutes);
+  // Items
+  app.get('/lists/:listID/items', requireAuth, itemController.getItems);
+  app.post('/lists/:listID/items', requireAuth, itemController.newItem);
+  app.get('/lists/:listID/items/:itemID', requireAuth, itemController.getItems);
+  app.delete('/lists/:listID/items/:itemID', requireAuth, itemController.deleteItem);
 };
